20bc2c7d9129f9d225f44a7cc94b412595a48201
Natalie <natalie.soltis@outlook.com>
2022-02-24 13:07:51 -0500

    Started work on managers

---
+++ Source/Collapse/GunManager.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GunManager.h"
+
+GunManager::GunManager()
+{
+}
+
+GunManager::~GunManager()
+{
+}
---
+++ Source/Collapse/GunManager.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+class Gun;
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class COLLAPSE_API GunManager
+{
+public:
+	GunManager();
+	~GunManager();
+
+private:
+
+};
